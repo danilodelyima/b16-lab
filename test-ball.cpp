b8cc1bdcb399ff41e70e55494f3046bd4a9c7dc8
Danilo de Lima <danilodelyima@gmail.com>
2017-04-30 15:18:20 -0300

    Implementa função run

--- test-ball.cpp
+++ test-ball.cpp
@@ -2,17 +2,17 @@
  ** brief: Tests the bouncing ball simulation
  ** author: Andrea Vedaldi
  **/
-
 #include "ball.h"
+
+void run(Simulation& s, double dt) {
+	for (int i = 0 ; i < 100 ; ++i) { s.step(dt) ; s.display() ; }
+}
 
 int main(int argc, char** argv)
 {
   Ball ball ;
   const double dt = 1.0/30;
   ball.setBall(0.5,0.5);
-  for (int i = 0 ; i < 100 ; ++i) {
-    ball.step(dt) ;
-    ball.display() ;
-  }
+  run(ball, dt);
   return 0 ;
 }
