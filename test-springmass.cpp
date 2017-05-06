077978de713fec8e0580cd440df6f72185d68d03
Danilo de Lima <danilodelyima@gmail.com>
2017-05-05 23:34:04 -0300

    Implementa test-springmass

--- test-springmass.cpp
+++ test-springmass.cpp
@@ -12,11 +12,16 @@ int main(int argc, char** argv)
   const double mass = 0.05 ;
   const double radius = 0.02 ;
   const double naturalLength = 0.95 ;
+  const double stiffness = 0.3;
+  const double damping = 0.01;
 
   Mass m1(Vector2(-.5,0), Vector2(), mass, radius) ;
   Mass m2(Vector2(+.5,0), Vector2(), mass, radius) ;
 
-/* INCOMPLETE: TYPE YOUR CODE HERE */
+  springmass.addMass(m1);
+  springmass.addMass(m2);
+  springmass.newSpring(0, 1, naturalLength, damping, stiffness);
+
 
   const double dt = 1.0/30 ;
   for (int i = 0 ; i < 100 ; ++i) {
